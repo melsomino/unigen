933fc9e0040cd7f76c6ce82be5f08c67093914bb
Michael Vlasov <melsomino@gmail.com>
2016-07-25 20:27:26 +0500

    remove out

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,13 @@
 import org.unified.dev.server.Dev_server;
 
 import java.nio.file.Paths;
-import java.util.Map;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
 			Dev_server dev_server = new Dev_server();
-			dev_server.dev_web_path = Paths.get("/Users/mu.vlasov/projects/unigen/src/org/unified/dev/server/web");
+			dev_server.dev_web_path = Paths.get("/Users/vlasov/projects/unified/unigen/src/org/unified/dev/server/web");
 			dev_server.configure(Paths.get(args[0]));
 			dev_server.run();
 		} catch (Exception e) {
